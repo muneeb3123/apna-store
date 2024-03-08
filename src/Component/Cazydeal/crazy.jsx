44d0f24c52586f4d0e73a7ded05bbc3a7dfe7c2e
Javed-Shahbaz <javedshahbaz007@gmail.com>
2024-03-08 23:27:18 +0500

    finalize crazy deal

--- src/Component/Cazydeal/crazy.jsx
+++ src/Component/Cazydeal/crazy.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import ratings from "../../assets/icon.svg";
+import dealimg from "../../assets/image.jpg";
+import cimg1 from "../../assets/image1.jpg";
+import cimg2 from "../../assets/image2.jpg";
+import cimg3 from "../../assets/image3.jpg";
+import "./crazy.css";
 export const Crazy = () => {
   return (
     <div>
@@ -15,50 +20,57 @@ export const Crazy = () => {
       </div>
       <div className="details">
         <div className="crazy-img">
-          <img src="" alt="Crazy Image" />
+          <img src={dealimg} alt="Crazy Image" />
         </div>
         <div className="deal-info">
           <div className="deal-title">
-            <h2>Class Time Boy's Short Sleeve Pique Polo Shirt</h2>
+            <h2>
+              Class Time Boy's Short Sleeve Pique <br />
+              Polo Shirt
+            </h2>
           </div>
           <div className="ratings">
             <img src={ratings} alt="image" />
-            <span>42 Reviews</span>
+            <span className="rc">42 reviews</span>
           </div>
           <div className="price">
-            <span className="sp">Rs.599</span>
-            <span>Rs.199</span>
+            <span className="s">Rs 599.00</span>
+            <span className="s1">Rs 199.00</span>
             <p>Sale</p>
           </div>
           <div className="colors">
             <span>color:White</span>
             <div className="color-images">
-              <img src={ratings} />
-              <img src={ratings} />
-              <img src={ratings} />
-              <img src={ratings} />
+              <img src={cimg1} />
+              <img src={cimg2} />
+              <img src={cimg3} />
+              <img src={dealimg} />
             </div>
             <div className="size">
               <span>size: 4 Years</span>
               <div className="sizes">
-                <button>4 Years</button>
-                <button>5-6 Years</button>
-                <button>7 Years</button>
-                <button>8 Years</button>
-                <button>10-12 Years</button>
-                <button>14-16 Years</button>
+                <button type="button"> 4 Years</button>
+                <button type="button">5-6 Years</button>
+                <button type="button">7 Years</button>
+                <button type="button">8 Years</button>
+                <button type="button">10-12 Years</button>
+                <button type="button">14-16 Years</button>
               </div>
-              <div className="quantiry">
-                <h4>Quantity</h4>
-                <button>- 1 +</button>
+            </div>
+            <div className="quantity">
+              <h4>Quantity</h4>
+              <div className="sp">
+                <span> - </span>
+                <span> 1 </span>
+                <span> + </span>
               </div>
-              <div className="cart">
-                <button>Add to Cart</button>
-              </div>
-              <div className="share-details">
-                <h5>Share</h5>
-                <h4>View full details</h4>
-              </div>
+            </div>
+            <div className="cart">
+              <button type="button">Add to Cart</button>
+            </div>
+            <div className="share-details">
+              <span>Share</span>
+              <span>View full details</span>
             </div>
           </div>
         </div>
