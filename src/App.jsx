a33045860b229e8e72e40ea1579dbc6faa8848ef
daudb6 <daudbasharat6@gmail.com>
2024-03-12 22:18:38 +0500

    footer Complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Productslider } from "./Component/Productslider";
+import { Footer } from "./Component/footer/Footer";
 
 function App() {
   return (
     <>
-      <Productslider/>
+      {/* <Productslider/> */}
+      <Footer />
       </>
   )
 }
